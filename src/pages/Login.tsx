a1e6e9134b92ab94fc25f0b907fd231587680b60
Ekramul <mdekramulhassan168@gmail.com>
2024-07-06 11:04:12 +0600

    login page code added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,38 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+
 const Login = () => {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const [login, { isError }] = useLoginMutation();
+  console.log({ isError });
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    console.log(data);
+    const res = await login(userInfo).unwrap();
+    dispatch(setUser({ user: {}, token: res.data.accessToken }));
+    console.log(res);
+  };
   return (
-    <div>
-      <h1>This is Login page</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID</label>
+        <input type="text" id="id" {...register("id")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">login</Button>
+    </form>
   );
 };
 
